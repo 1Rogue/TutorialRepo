1d179c239d8aa70894d457595cc9a4c7f3e10ecb
Spencer Alderman <spencer@1rogue.com>
2013-11-09 15:03:20 -0500

    init commit

---
+++ src/main/java/com/rogue/tutorialrepo/TutorialRepo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rogue.tutorialrepo;
+
+/**
+ *
+ * @since 1.0.0
+ * @author Spencer
+ * @version 1.0.0
+ */
+public class TutorialRepo {
+    
+    public static void main (String[] args) {
+        System.out.println("This is a sample project!");
+    }
+    
+}
